5ed370f5fe5f2f2c6cf06ac621b437fb2a19911e
Rainbow <494738974@qq.com>
2026-09-10 16:51:38 +0800

    fix(deploy): export Harness home in container

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -eu
 
 dsh_home="${DOCGUARD_DSH_HOME:-/var/lib/docguard/dsh}"
 skills_dir="${dsh_home}/skills"
+export DSH_HOME="$dsh_home"
 
 mkdir -p "$skills_dir"
 ln -sfn /app/doc-audit-integrate-skill \
